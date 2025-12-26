5fa8421dfee3d200e301e8dab0caf129e4aa9338
AaryanVerma007 <aaryanudyalak@gmail.com>
2025-12-26 16:01:22 +0530

    remove tensorflow (not supported on python 3.13)

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from transformers import BertTokenizer, TFBertForSequenceClassification
-import tensorflow as tf
 import numpy as np
 
 # ---------- LOAD MODEL ----------
